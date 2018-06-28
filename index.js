68d9e17e7e50a36b4b598a8b2ed1751105e37915
jianguo.mu <980751937mu@gmail.com>
2018-06-28 16:02:29 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const path = require('path');
+const randomstring = require('randomstring');
+const argv = process.argv;
+
+let file = argv[2];
+
+const wordArr = [];
+const totalArr = [];
+// console.log('file', argv);
+if (!file) {
+  console.error('⚠️ ⚠️ ⚠️ the file path must be proived');
+  process.exit();
+}
+
+const pathArr = []
+const readDir = (path) => {
+const exists = fs.existsSync(path);
+const stat = fs.statSync(path);
+  if (exists && stat) {
+    if (stat.isFile()) {
+      pathArr.push(path)
+      return;
+    }
+
+    const handleFile = fs.readdirSync(path);
+    if (handleFile && handleFile.length) {
+      handleFile.forEach(item => {
+        readDir(`${path}/${item}`)
+      })
+    }
+  }
+}
+
+readDir(`${process.cwd()}/${file}`)
+
+const write = [];
+pathArr.forEach((item, i) => {
+if (!(['.js', '.jsx'].indexOf(path.extname(item)) > -1)) return;
+  let pathStr;
+  let ext = ['.js', '.jsx'][['.js', '.jsx'].indexOf(path.extname(item))];
+  pathStr = path.basename(item, ext)
+  if (pathStr === 'index') {
+    pathStr = (((path.parse(item)).dir).split(path.sep)).pop();
+  }
+
+  const data = fs.readFileSync(item, "utf8");
+  let gData = filterText(data, pathStr);
+
+  fs.writeFileSync(item, gData, 'utf8');
+})
+
+
+if (pathArr.length) {
+  console.log();
+  console.log('✅ 国际化文件替换成功');
+  wordArr.forEach((o) => {
+    const value = `"${o.key}": "${o.value}",`
+    write.push(value)
+  });
+  const str = `{ ${write.join("\n")} }`
+  fs.writeFileSync('i18n.json', str, 'utf8');
+  console.log();
+  console.log('✅ 国际化词条整理成功');
+}
+
+function filterText(text, objName) {
+  let pattern = /("([^\\\"]*(\\.)?)*")|('([^\\\']*(\\.)?)*')|(\/{2,}.*?(\r|\n))|(\/\*(\n|.)*?\*\/)|((=)?(\s+)?(|`|"|')?(\s+)?[\u4e00-\u9faf]+('|"|`)?)/g
+  return text.replace(pattern, (word) => {
+    if (!(/^\/{2,}/.test(word) ||  /^\/\*/.test(word)) && /[\u4e00-\u9faf]+/g.test(word)) {
+      let uuid = randomstring.generate(4);
+      let currentObj = []
+      let str
+      if (totalArr.length && totalArr.indexOf((word.match(/[\u4e00-\u9faf]+/g))[0])) {
+        currentObj =  wordArr.filter(item => item.value === (word.match(/[\u4e00-\u9faf]+/g))[0])
+      }
+
+      if (currentObj.length) {
+        str = `intl.get('${currentObj[0].key}')`;
+      } else {
+        str = `intl.get('${objName}_i18n_${uuid}')`;
+        totalArr.push((word.match(/[\u4e00-\u9faf]+/g))[0]);
+        wordArr.push({
+          key: `${objName}_i18n_${uuid}`,
+          value: `${(word.match(/[\u4e00-\u9faf]+/g))[0]}`,
+        })
+      }
+      
+      if (/=/.test(word)) return word.replace(/(`|"|')?(\s+)?[\u4e00-\u9faf]+(\s+)?('|"|`)?/, `\{${str}\}`);
+      if (/(`|"|')(\s+)?[\u4e00-\u9faf]+(\s+)?('|"|`)/.test(word)) return word.replace(/(`|"|')?(\s+)?[\u4e00-\u9faf]+(\s+)?('|"|`)?/, `${str}`);
+      return word.replace(/[\u4e00-\u9faf]+/, `\{${str}\}`);
+    }
+
+    return word
+  })
+}
---
+++ test/components/index.jsx
@@ -0,0 +1,108 @@
+
+
+/**
+ * 该脚本不具备任何意义，是我从别处随便找的，只是用来测试替换中文 & 收集词条。
+ * 😂😂😂😂😂  勿喷！！！
+ */
+import React from 'react';
+import { Form, Input, Modal, message} from 'antd';
+import intl from 'react-intl-universal';
+import { getDataDirect} from '../../../redux/actions/getDataFromServerAciton';
+const FormItem = Form.Item;
+
+export default Form.create()((props) => {
+  const { visible, form, onCancel, onOk, title } = props;
+  const { getFieldDecorator } = form;
+  const formItemLayout = {
+    labelCol: { span: 4},
+    wrapperCol: { span: 19 },
+  };
+
+  // 测试的脚本
+  function handleCreate() {
+    //const form = this.form;
+    // if(this.isType === 'new') {
+    //   sessionStorage.removeItem('taskName');
+    // }
+    form.validateFields((err, values) => {
+      if(err) {
+        return;
+      }
+      let path = '/task/create?userId=1';
+      for(let p in values) {
+        path += '&' + p + '=' + values[p];
+      }
+
+      getDataDirect(path).then((response) => {
+        if(response.data.retcode == 200) {
+          window.bfd.taskInformation = response.data.data;
+          onCancel();
+          if(!window.bfd.isClearCan) {
+            onOk([], [], response.data.data);
+            
+          }
+          //message.success(response.data.retdesc)
+        }
+        if(response.data.retcode != 200) {
+          message.destroy();
+          message.warning(response.data.retdesc);
+          return false;
+        }
+        if(window.bfd.isInit) {
+          window.bfd.isInit = false;
+          window.bfd.isClearCan = false;
+          window.bfd.handleClickSave&&window.bfd.handleClickSave();
+        }
+      });
+    });
+  }
+  /**
+   * 测试注释块
+   * 测试注释块
+   * 测试注释块
+   * 测试注测试注释块释块
+   * 测试注释块
+   */
+  const ss = [{
+    title: "姓名"
+  },{
+    age: "年龄"
+  },{
+    id: "编号"
+  },{
+    job: "工作"
+  }]
+  return (
+    <Modal
+      width={700}
+      visible={visible}
+      title={title}   //测试注释
+      okText="确定"  //完成并创建
+      cancelText={intl.get("index._i18n_4OX3")}   //取消
+      onOk={handleCreate}
+      onCancel={onCancel}
+    >
+      <div className="newForm">
+        {/*创建分析作业用于保存共享*/}
+        <div className="hint">首页</div>
+        <Form style={{width: '80%', margin: '0 auto'}}>
+          {/*作业名称*/}
+          德德, 姓名
+          <FormItem label="表单" {...formItemLayout}>
+            {getFieldDecorator('taskName', {
+              //请输入作业名称
+              rules: [
+                {required: true, whitespace: true, message: "不能为空" },
+                {max: 50, message: '不能不填'}
+              ],
+            })(
+              <Input />
+            )}
+          </FormItem>
+          {/*作业简介*/}
+          页面底部
+        </Form>
+      </div>
+    </Modal>
+  );
+});
